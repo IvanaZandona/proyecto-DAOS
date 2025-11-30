5858041d00b6ab74ece846b976d504099c409896
Lucas <122121326+lucas-110504@users.noreply.github.com>
2025-11-30 16:47:08 -0300

    Update CiudadRepository.java

--- src/main/java/accesoDatos/CiudadRepository.java
+++ src/main/java/accesoDatos/CiudadRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import entidades.Ciudad;
 
 public interface CiudadRepository extends JpaRepository<Ciudad, Long> {
+  Ciudad findByNombre(String nombre);
 }
+
