bddc8ff87d9e76ed437caf823533763a8e85c003
Lucas <122121326+lucas-110504@users.noreply.github.com>
2025-11-30 16:49:51 -0300

    Update CiudadRestController.java

--- src/main/java/presentacion/CiudadRestController.java
+++ src/main/java/presentacion/CiudadRestController.java
@@ -1,1 +1,35 @@
+package presentacion;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import entidades.Ciudad;
+import servicios.CiudadService;
+
+@RestController
+@RequestMapping("/ciudades")
+public class CiudadRestController {
+
+    @Autowired
+    private CiudadService ciudadService;
+
+    @GetMapping
+    public ResponseEntity<List<Ciudad>> listarTodas() {
+        return ResponseEntity.ok(ciudadService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+        Optional<Ciudad> ciudadOpt = ciudadService.getById(id);
+
+        if (ciudadOpt.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(ciudadOpt.get());
+    }
+}
 
